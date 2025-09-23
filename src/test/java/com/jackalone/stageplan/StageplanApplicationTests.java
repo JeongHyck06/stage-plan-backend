edaecdf71bda74cd80a6f847519b7a10fcd4d42d
Jack <devjack050700@gmail.com>
2025-09-24 03:12:12 +0900

    프로젝트 생성

---
+++ src/test/java/com/jackalone/stageplan/StageplanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackalone.stageplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StageplanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
