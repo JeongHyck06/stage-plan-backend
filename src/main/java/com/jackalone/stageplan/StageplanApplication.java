31d0422a0e5535003c7f681ba45975b6da5b5a05
Jack <devjack050700@gmail.com>
2025-09-24 03:15:50 +0900

    프로젝트 생성

---
+++ src/main/java/com/jackalone/stageplan/StageplanApplication.java
@@ -0,0 +1,13 @@
+package com.jackalone.stageplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StageplanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StageplanApplication.class, args);
+	}
+
+}
